693a484349a7362a8cf19ff617f0335f13e48276
Akpodigha Filatei <filatei@gtsng.com>
2020-06-06 17:58:12 +0100

    customer unique name

--- routes/customers.js
+++ routes/customers.js
@@ -128,10 +128,10 @@ router.post("/import", (req, res, next) => {
 
     let customer = new Customer(el);
     customer.barcode = customer._id + customer.name.trim();
-    console.log(customer);
-
+   
     customer.save()
     .then(result => {
+      console.log(result, ' added');
     
       
     })

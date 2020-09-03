5572857746ebcee326f21f305b5edf0f498571e3
Akpodigha Filatei <filatei@gtsng.com>
2020-09-03 17:14:26 +0100

    socket.io fixes 3

--- app.js
+++ app.js
@@ -61,6 +61,7 @@ const allowedOrigins = [
     'capacitor://localhost',
     'ionic://localhost',
     'http://localhost',
+    'https://posclaims.torama.ng',
     
     'http://localhost:8080',
     'http://localhost:8100',

029385acd51c7a068e94f0e69eb74ef5cbc847b8
Akpodigha Filatei <filatei@gtsng.com>
2020-09-05 13:45:31 +0100

    expense mail change

--- routes/expense.js
+++ routes/expense.js
@@ -41,7 +41,7 @@ async function sendMail(expense) {
  let expenseId = expense.expense_id
 
  expenseId = expenseId.toString().padStart(5, '0')
- let subject = `ShopTorama Expense Confirmation for (# ${expenseId})`
+ let subject = `Expense Confirmation for (# ${expenseId})`
  let curr =  process.env.naira
  let total = expense.txn_amount;
  let status = expense.status;
@@ -70,7 +70,7 @@ async function sendMail(expense) {
  product += `</tbody><tfoot><tr><td colspan="4" style="text-align:right;" > Sum: ${derived_total} ${curr}</td></tr></tfoot></table>`;
 
  let html = `<!DOCTYPE html><html><body style="text-align:center;"><img src="${logo}" alt="logoimg" width="50"><p style="background:rgba(0, 128, 0,0.051); text-align:center;">
-            ${date}</p><h2>EXPENSE CONFIRMED</h2><p> Hi ${userName},</p>`;
+            ${date}</p><h2>EXPENSE ${status}</h2><p> Hi ${userName},</p>`;
  html += `<p>The Status of your Expense Request # ${expenseId} for ${curr} ${total.toLocaleString()}  is now ${status}</p><p>Vendor: ${vendor}</p> <p>Factory Location: ${location}</p>`;
  html += `Product: ${product}`;
  html += `<table style="margin-left:auto; margin-right:auto"><tr style="text-align:left;"><td><h3>Expense summary</h3></td></tr><tr style="text-align:left;"><td>Status:</td><td> ${status}</td></tr><tr style="text-align:left;"><td> Subtotal:</td><td> ${curr} ${total.toLocaleString()} </td></tr>

9a5ea43218ab6d22501e4a97f896586b66256ee6
Akpodigha Filatei <filatei@gtsng.com>
2020-11-08 08:17:55 +0100

    payhist in mail 5

--- mail.js
+++ mail.js
@@ -209,7 +209,9 @@ async function sendExpense(expense) {
   html += `<table style="margin-left:auto; margin-right:auto"><tr style="text-align:left;"><td><h3>Expense summary</h3></td></tr><tr style="text-align:left;"><td>Status:</td><td> ${status}</td></tr><tr style="text-align:left;"><td> Subtotal:</td><td> ${curr} ${total.toLocaleString()} </td></tr>
     <tr style="text-align:left;"> <td>Paid: </td><td>${curr} ${paidAmount}</td></tr> <tr style="text-align:left;"><td>Balance: </td><td>${curr} ${balance.toLocaleString()}</td></tr></table>`;
 
-  html += `<h3>Pay History</h3><table style="margin-left:auto; margin-right:auto"> ${payHist} </table>`;
+  if (payHist) {
+    html += `<h3>Pay History</h3><table style="margin-left:auto; margin-right:auto"> ${payHist} </table>`;
+  }
 
   html += `<p> Remarks: ${remarks} </p>`;
   if (memo) {

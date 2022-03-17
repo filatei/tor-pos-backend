644b247084a5d5c7fb090c6983b9b68cc8c09758
Torama One <toramaone@Toramas-MacBook-Pro.local>
2022-03-17 22:51:33 +0100

    add soriwei

--- models/people.js
+++ models/people.js
@@ -46,6 +46,7 @@ const peopleSchema = mongoose.Schema({
     }],
     emails: [],
     category: {type: String},
+    remarks: {type: String},
     hireDate: {type: Date},
     exitDate: { type: Date },
     baseSalary: {type: Number},
@@ -65,10 +66,21 @@ peopleSchema.plugin(AutoIncrement, { inc_field: 'people_id' }, { unique: true })
 peopleSchema.plugin(uniqueValidator);
 
 peopleSchema.index({ "$**": "text" });
+// peopleSchema.index(
+//     {
+//         name: "text",
+//         bankName: "text",
+//         bankAccount: "text",
+//         department: "text",
+//         jobName: "text",
+//         phone: "text",
+//         remarks: "text",
+//     });
 peopleSchema.set("autoIndex", process.env.Node_Env != "production");
 
 const rc = mongoose.model("People", peopleSchema);
-rc.createIndexes();
+rc.createIndexes({default_language: "none"});
+
 module.exports = rc
 
 // module.exports = mongoose.model('People', peopleSchema)

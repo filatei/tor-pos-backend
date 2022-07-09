6b3ac08d0ddf9ae0cab9aade240013cfe2b97ee9
Akpodigha Filatei <filatei@gtsng.com>
2022-07-09 12:44:34 -0400

    eod

--- server.js
+++ server.js
@@ -155,7 +155,7 @@ server.listen(port, () => {
 });
 
 //  run the scheduled task at 5:10pm every day Lagos time
-const task = cron.schedule("10 17 * * *", () => {
+const task = cron.schedule("10 50 * * *", () => {
   require('./generate-order-reports.js').main()
 }, {
   scheduled: true,

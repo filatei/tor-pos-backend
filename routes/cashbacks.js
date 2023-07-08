86bfa2e9897cfdba3ba3a1cb7a50fad892dcaf96
user1 <userone@user1s-MacBook-Pro.local>
2023-07-08 09:31:59 +0100

    put back update cashback directly

--- routes/cashbacks.js
+++ routes/cashbacks.js
@@ -74,4 +74,51 @@ router.post("", checkAuth, upload.single("image"), async (req, res) => {
   }
 });
 
+router.put(
+  "/:id",
+  checkAuth,
+  upload.single("image"),
+  async (req, res) => {
+    try {
+      const DOMAIN =
+        process.env.DOMAIN || req.protocol + "://" + req.get("host");
+
+      const { id } = req.params;
+      const { imageText } = req.body;
+      let toUpdate = { ...req.body, updatedBy: req.userData.userId };
+
+      if (req.file?.path) {
+        const url = HOSTNAME.includes("torama.ng")
+          ? "https://fido-api.torama.ng"
+          : DOMAIN;
+        const paymentProofImage =
+          url +
+          "/cashbackUploads/" +
+          req.file.path.split("/var/www/uploads/cashbackimages")[1];
+        toUpdate = { ...toUpdate, paymentProofImage };
+      }
+
+      const updatedCashback = await CashBack.findByIdAndUpdate(id, toUpdate, {
+        new: true,
+      });
+
+      if (!updatedCashback) {
+        return res.status(404).json({ message: "Cashback not found." });
+      }
+      console.log(updatedCashback, "updatedCashback");
+
+      return res.status(200).json({
+        record: updatedCashback,
+        message: "Cashback updated successfully.",
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: `Server error: ${error.message}` }); // show error message for better debugging
+    }
+  }
+);
+
+
+
+
 module.exports = router;

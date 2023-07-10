64f656c245b098e4113d4aca0b77953a41159f00
user1 <userone@user1s-MacBook-Pro.local>
2023-07-10 16:26:56 +0100

    change merge on to remove calc fields

--- models/cashback.js
+++ models/cashback.js
@@ -1,12 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 // const Schema = mongoose.Schema;
 
-const CashBackSchema = new mongoose.Schema({
+const CashBackSchema = new mongoose.Schema(
+  {
     customerName: { type: String, required: true },
     productName: { type: String, required: true },
-    customerId: {type: mongoose.Schema.Types.ObjectId, ref: 'Customer', required: true },
-    attachId: {type: mongoose.Schema.Types.ObjectId, ref: 'CashBackAttach',  },
-   
+    customerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Customer",
+      required: true,
+    },
+    attachId: { type: mongoose.Schema.Types.ObjectId, ref: "CashBackAttach" },
+
     site: { type: String, required: true },
     totalQty: { type: Number, required: true },
     totalSalesSum: { type: Number, required: true },
@@ -14,24 +19,32 @@ const CashBackSchema = new mongoose.Schema({
     startDate: { type: Date, required: true },
     endDate: { type: Date, required: true },
 
-    status: { type: String, enum: ['PAID', 'UNPAID'], default: 'UNPAID' },
+    status: { type: String, enum: ["PAID", "UNPAID"], default: "UNPAID" },
 
     imageText: { type: String },
     paymentProofImage: { type: String }, // Assuming this will be an URL to the image
-    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    updatedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
-}, {
-    timestamps: true
-}); // This enables automatic timestamp generation
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    updatedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+); // This enables automatic timestamp generation
 
 CashBackSchema.index({
-    customerId: 'text', site: "text", startDate: "text",
-    endDate: "text",
-    productName: 'text',
-    totalQty: 'text',
-    totalSalesSum: 'text',
-    specialSalesSum: 'text'
+  customerId: "text",
+  site: "text",
+  startDate: "text",
+  endDate: "text",
+  productName: "text",
 });
 
-
-module.exports = mongoose.model('CashBack', CashBackSchema);
+module.exports = mongoose.model("CashBack", CashBackSchema);

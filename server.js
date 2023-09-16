c9ba2508b271e714daaedd0122220ad1041da867
user1 <userone@user1s-MacBook-Pro.local>
2023-09-16 18:09:02 +0100

    web socket

--- server.js
+++ server.js
@@ -29,63 +29,69 @@ mongoEmitter.on('mongoConnected', (db) => {
   const changeStream = orderCollection.watch();
 
   changeStream.on('change', async (change) => {
-    const fullDocument = change.fullDocument;
-    console.log(' inside changestream')
-    if (!fullDocument) return;
 
-    const transDate = new Date(fullDocument.trans_date);
+    const operationType = change.operationType;
+
+    if (operationType === 'delete') {
+      // Handle delete operation
+      console.log('A document was deleted', change);
+      // Your code for handling deletes
+    }
+    // const fullDocument = change.fullDocument;
+    console.log(' inside changestream')
+    // if (!fullDocument) return;
+
+    // const transDate = new Date(fullDocument.trans_date);
     const today = new Date();
     today.setHours(0, 0, 0, 0);
 
-    if (transDate >= today) {
-      // Run aggregation query for today's sales per site
-      const tomorrow = new Date(today);
-      tomorrow.setDate(tomorrow.getDate() + 1);
+    // if (transDate >= today) {
+    // Run aggregation query for today's sales per site
+    const tomorrow = new Date(today);
+    tomorrow.setDate(tomorrow.getDate() + 1);
 
-      const aggregationResults = await orderCollection.aggregate([
-        {
-          $match: {
-            trans_date: { $gte: today, $lt: tomorrow },
-            // site: fullDocument.site  // Change this if you want aggregation for all sites
-          }
-        },
-        {
-          $unwind: '$products'
-        },
-        {
-          $group: {
-            _id: {
-              site: '$site',
-              productName: '$products.name'
+    const aggregationResults = await orderCollection.aggregate([
+      {
+        $match: {
+          trans_date: { $gte: today, $lt: tomorrow },
+          // site: fullDocument.site  // Change this if you want aggregation for all sites
+        }
+      },
+      {
+        $unwind: '$products'
+      },
+      {
+        $group: {
+          _id: {
+            site: '$site',
+            productName: '$products.name'
+          },
+          totalQty: { $sum: '$products.qty' },
+          totalAmount: { $sum: '$products.amount' }
+        }
+      },
+      {
+        $group: {
+          _id: "$_id.site",
+          products: {
+            $push: {
+              productName: "$_id.productName",
+              totalQty: "$totalQty",
+              totalAmount: "$totalAmount",
             },
-            totalQty: { $sum: '$products.qty' },
-            totalAmount: { $sum: '$products.amount' }
-          }
-        },
-        {
-          $group: {
-            _id: "$_id.site",
-            products: {
-              $push: {
-                productName: "$_id.productName",
-                totalQty: "$totalQty",
-                totalAmount: "$totalAmount",
-              },
-            },
-          }
-        },
-      ]).toArray();
+          },
+        }
+      },
+    ]).toArray();
 
-      // Broadcast the summary data to all WebSocket clients
-      console.log(aggregationResults, ' aggregationResults')
-      wss.clients.forEach(client => {
-        client.send(JSON.stringify(aggregationResults));
-      });
-    }
+    // Broadcast the summary data to all WebSocket clients
+    console.log(aggregationResults, ' aggregationResults')
+    wss.clients.forEach(client => {
+      client.send(JSON.stringify(aggregationResults));
+    });
+    // }
   });
 });
-
-
 
 const normalizePort = (val) => {
   var port = parseInt(val, 10);
